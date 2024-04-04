14d0c4f4960a77627e41746d506931b82b865c72
sivasuthan manojan <mjcode6@gmail.com>
2024-04-04 12:34:12 +0200

    I'm starting front-end angular and i made springboot, angular app ssuccessfully up and running and so on notably i can able to bring product list on angular front-end and now i will move towards html for get greate template. Bye see you soon!

--- 02-backend/spring-boot-ecommerce/src/main/java/com/mjCode6/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/mjCode6/ecommerce/dao/ProductRepository.java
@@ -2,8 +2,9 @@ package com.mjCode6.ecommerce.dao;
 
 import com.mjCode6.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
---
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { response } from 'express';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private basrUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  getProductList(): Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(this.basrUrl).pipe(map(response => response._embedded.products));
+  }
+}
+
+
+interface GetResponse{
+  _embedded: {
+    products: Product[];
+  }
+}
