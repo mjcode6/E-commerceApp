46caf316bde3db4d5a2b90d907aa578b9d163e6a
sivasuthan manojan <mjcode6@gmail.com>
2024-04-24 14:06:33 +0200

    so i made a little update so that we call as dependent fields for ajusting month for year's between cuirrent year and future years so that it for today but dont't forget i'm going work on populate countries and state for tommorrow so HAppy coding ...

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -111,6 +111,30 @@ this.x8ShoppingFormService.getCreditCardYears().subscribe(
   }
 
 
+  handleMonthsAndYears(){
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
 
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+
+    // if current year equels the selected year , then start with current month
+
+    let startMonth: number;
+
+    if(currentYear === selectedYear){
+      startMonth = new Date().getMonth()+ 1;
+    }else{
+      startMonth = 1;
+    }
+
+
+    this.x8ShoppingFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrived credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
   
 }
